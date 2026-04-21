53c88078656c36de8fcf9097309ed71fb87c3096
if3lc <e-ben@msn.com>
2026-04-21 03:10:33 +0300

    Initial Commit

---
+++ NoteDad/NoteDadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteDadApp.swift
+//  NoteDad
+//
+//  Created by ismail ihsan bülbül on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct NoteDadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
